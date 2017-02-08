2b2aa21421e29b0fa1a0b1b5046f38fb177d8794
Aleksandr Yeganov <ayeganov@gmail.com>
2017-02-07 20:16:29 -0500

    Add bot brain to JS

--- web/app/brain.js
+++ web/app/brain.js
@@ -1,7 +1,7 @@
 'use strict';
-define([], function()
+define(['underscore'], function(_)
 {
-    class Brain
+    class PlayerBrain
     {
         constructor()
         {
@@ -69,7 +69,89 @@ define([], function()
         }
     };
 
+
+    class BotBrain
+    {
+        constructor(json_net)
+        {
+            this.neurons = this._restore_links(json_net);
+        }
+
+        _restore_links(neurons)
+        {
+            var get_neuron_ptr = function(nid)
+            {
+                for(let neuron of neurons)
+                {
+                    if(neuron.ID === nid)
+                    {
+                        return neuron;
+                    }
+                }
+                throw "Invalid network file: Referenced neuron ID " + nid + " doesn't have a neuron object.";
+            };
+
+            for(let neuron of neurons)
+            {
+                neuron.OutputSignal = 0.0;
+                for(let link of neuron.InLinks)
+                {
+                    link.In = get_neuron_ptr(link.InputID);
+                }
+
+                for(let link of neuron.OutLinks)
+                {
+                    link.In = get_neuron_ptr(link.OutputID);
+                }
+            }
+            return neurons;
+        }
+
+        _activation_function(value, activation_response)
+        {
+            return (1.0 / (1.0 + Math.exp(-4.9 * value)));
+        }
+
+        update(sensor_readings)
+        {
+            var outputs = [];
+            var neuron_idx = 0;
+
+            while(this.neurons[neuron_idx].Type === "INPUT")
+            {
+                this.neurons[neuron_idx].OutputSignal = sensor_readings[neuron_idx];
+                ++neuron_idx;
+            }
+
+            // set the bias neuron output to 1
+            this.neurons[neuron_idx++].OutputSignal = 1;
+
+            while(neuron_idx < this.neurons.length)
+            {
+                var cur_neuron = this.neurons[neuron_idx];
+                var pre_activations = _.map(cur_neuron.InLinks, function(link, idx)
+                {
+                    return link.Weight * link.In.OutputSignal;
+                });
+
+                var sum = _.reduce(pre_activations, function(memo, output) { return memo + output; }, 0);
+
+                cur_neuron.OutputSignal = this._activation_function(sum, cur_neuron.ActivationResponse);
+
+                if(cur_neuron.Type === "OUTPUT")
+                {
+                    outputs.push(cur_neuron.OutputSignal);
+                }
+                ++neuron_idx;
+            }
+
+            return outputs;
+        }
+
+    };
+
     return {
-        Brain: Brain
+        PlayerBrain: PlayerBrain,
+        BotBrain: BotBrain
     }
 });
